ec3226c9e883d0653c88168c4c9d39123f369964
Divyansh Arora <76780136+DivyanshArora246@users.noreply.github.com>
2021-08-30 08:24:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ Webcam.set({
     width: 350,
     height: 300,
     image_format: 'png',
-    png_quality: 90
+    png_quality: 150
 });
 camera = document.getElementById("camera");
 Webcam.attach('#camera');
